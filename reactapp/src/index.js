0b2c311f5e1bd9b2c9cc9d3dd70d256cd4aacc12
xieyu <1140236905@qq.com>
2018-09-07 18:34:38 +0800

    1

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './comment/index.css';
 // import App from './App';
 import CommentApp  from './comment/CommentApp';
 import registerServiceWorker from './registerServiceWorker';
