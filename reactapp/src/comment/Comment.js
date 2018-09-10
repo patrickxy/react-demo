2ea45a698553812d263de20d3c2e90010863c942
patrickxy <18200569466@163.com>
2018-09-10 23:24:11 +0800

    firstpart

---
+++ reactapp/src/comment/Comment.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+export default class Comment extends Component{
+    render(){
+        const {comment} = this.props;
+        return(
+            <div className="comment">
+                <div className='comment-user'>
+                    <span>{comment.username} </span>：
+                    </div>
+                    <p>{comment.content}</p>
+            </div>
+        )
+    }
+}
--- reactapp/src/comment/CommentApp.js
+++ reactapp/src/comment/CommentApp.js
@@ -3,11 +3,26 @@ import CommentInput from './CommentInput'
 import CommentList from './CommentList'
 
 class CommentApp extends Component {
+  constructor(){
+    super();
+    this.state = {
+      commentList : []
+    }
+  }
+  handleSubmitComment(comment){
+    if(!comment) return;
+    if(!comment.username) return alert('请输入用户名');
+    if(!comment.content) return alert('请输入评论内容');
+    this.state.commentList.push(comment)
+    this.setState({
+      commentList: this.state.commentList
+    })
+  }
   render() {
     return (
-      <div class="wrapper">
-        <CommentInput />
-        <CommentList />
+      <div className="wrapper">
+        <CommentInput onSubmit={this.handleSubmitComment.bind(this)} />
+        <CommentList comments = {this.state.commentList} />
       </div>
     )
   }
