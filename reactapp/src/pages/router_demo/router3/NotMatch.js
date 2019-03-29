0858f25d9d499fbf492ec48d269e347d1cfc398d
xieyu <1140236905@qq.com>
2019-03-29 15:35:57 +0800

    m

---
+++ reactapp/src/pages/router_demo/router3/NotMatch.js
@@ -0,0 +1,6 @@
+import React from 'react'
+export default class NotMatch extends React.Component {
+  render() {
+    return <div>这是not match 的路由</div>
+  }
+}
--- reactapp/src/pages/router_demo/router3/router.js
+++ reactapp/src/pages/router_demo/router3/router.js
@@ -1,28 +1,34 @@
 import React from 'react'
-import { HashRouter as Router, Route } from 'react-router-dom'
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import Main from './main'
 import About from '../router1/about'
 import Info from './info'
 import Topic from '../router1/topic'
 import Home from './Home'
+import NotFound from './NotMatch'
 export default class IRouter extends React.Component {
   render() {
     return (
       <Router>
         <Home>
-          {/* <Route path="/" component={Main} exact /> */}
-          <Route
-            path="/main"
-            // exact={true}
-            render={() => (
-              <Main>
-                <Route path="/main/:mainId" component={Info} />
-                {/* 动态参数 */}
-              </Main>
-            )}
-          />
-          <Route path="/about" component={About} />
-          <Route path="/topics" component={Topic} />
+          <Switch>
+            {/* 重定向 */}
+            <Route path="/" exact render={() => <Redirect to="/main" />} />
+            <Route
+              path="/main"
+              // exact={true}
+              render={() => (
+                <Main>
+                  <Route path="/main/:mainId" component={Info} />
+                  {/* 动态参数 */}
+                </Main>
+              )}
+            />
+            <Route path="/about" component={About} />
+            <Route path="/topics" component={Topic} />
+            <Route path="/topics" component={Topic} />
+            <Route component={NotFound} />
+          </Switch>
         </Home>
       </Router>
     )
