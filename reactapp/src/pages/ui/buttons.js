8237e24cec92cbbd489d0240b43d87eb84abafc5
xieyu <1140236905@qq.com>
2019-04-16 15:34:50 +0800

    m

--- reactapp/src/pages/ui/buttons.js
+++ reactapp/src/pages/ui/buttons.js
@@ -3,13 +3,59 @@
  * @created 2019/04/10 15:19:30
  */
 import React from 'react'
-import { Card, Button } from 'antd'
+import { Card, Button, Radio } from 'antd'
 export default class Buttons extends React.Component {
+  state = {
+    loading: false,
+    size: 'default'
+  }
+  enterLoading = () => {
+    this.setState({ loading: true })
+  }
+  cancelLoading = () => {
+    this.setState({ loading: false })
+  }
+  handleSizeChange = e => {
+    this.setState({ size: e.target.value })
+  }
   render() {
     return (
       <div>
         <Card>
-          <Button type="primary">创建</Button>
+          <Button type="primary" loading={true}>
+            创建
+          </Button>
+          <Button
+            type="primary"
+            loading={this.state.loading}
+            onClick={this.enterLoading}
+          >
+            Click me!
+          </Button>
+          <Button type="danger" onClick={this.cancelLoading}>
+            Cancel
+          </Button>
+          <Button.Group>
+            <Button
+              type="danger"
+              size={this.state.size}
+              onClick={this.cancelLoading}
+            >
+              Cancel
+            </Button>
+            <Button
+              type="danger"
+              size={this.state.size}
+              onClick={this.cancelLoading}
+            >
+              Cancel
+            </Button>
+          </Button.Group>
+          <Radio.Group value={this.state.size} onChange={this.handleSizeChange}>
+            <Radio value="small" />
+            <Radio value="default" />
+            <Radio value="large" />
+          </Radio.Group>
         </Card>
       </div>
     )
